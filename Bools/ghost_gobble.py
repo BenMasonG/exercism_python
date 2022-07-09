7caccb16f2b31cee1db52a80b7b5d78986e4ecab
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-07-09 23:21:58 +0100

    ghost_gobble code refactored. If/else statements replaced with simplified return statements that assess bool values.

--- Bools/ghost_gobble.py
+++ Bools/ghost_gobble.py
@@ -9,11 +9,7 @@ def eat_ghost(power_pellet_active, touching_ghost):
     :return: bool - can the ghost be eaten?
     """
 
-    if power_pellet_active is True and touching_ghost is True:
-        return True
-    else:
-        return False
-
+    return power_pellet_active and touching_ghost
 
 def score(touching_power_pellet, touching_dot):
     """Verify that Pac-Man has scored when a power pellet or dot has been eaten.
@@ -23,14 +19,10 @@ def score(touching_power_pellet, touching_dot):
     :return: bool - has the player scored or not?
     """
 
-    if touching_power_pellet is True or touching_dot is True:
-        return True
-    else:
-        return False
-
+    return touching_power_pellet or touching_dot
 
 def lose(power_pellet_active, touching_ghost):
-    """Trigger the game loop to end (GAME OVER) when Pac-Man touches a ghost without his 
+    """Trigger the game loop to end (GAME OVER) when Pac-Man touches a ghost without his
     power pellet.
 
     :param power_pellet_active: bool - does the player have an active power pellet?
@@ -38,11 +30,7 @@ def lose(power_pellet_active, touching_ghost):
     :return: bool - has the player lost the game?
     """
 
-    if power_pellet_active is False and touching_ghost is True:
-        return True
-    else:
-        return False
-
+    return not power_pellet_active and touching_ghost
 
 def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     """Trigger the victory event when all dots have been eaten.
@@ -53,8 +41,5 @@ def win(has_eaten_all_dots, power_pellet_active, touching_ghost):
     :return: bool - has the player won the game?
     """
 
-    if has_eaten_all_dots is True and (power_pellet_active is True or touching_ghost is False):
-        return True
-    else:
-        return False
-    
+    return has_eaten_all_dots and (power_pellet_active or not touching_ghost)
+ 
