6b2e03f19d28989385b05f031c627a831c0374c6
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-07-13 02:53:43 +0100

    reverse string exercise completed

---
+++ Loops/reverse_string.py
@@ -0,0 +1,12 @@
+'''
+The revervse string exercise.
+'''
+
+def reverse(text):
+    '''
+    Takes a string and outputs it in reverse.
+
+    param: text - string. Any string.
+    retrun: string - the reverse of the inputted string.
+    '''
+    return text[::-1]
