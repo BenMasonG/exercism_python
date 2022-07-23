5a8ff3eab111d0d74eed90a589339193532905c6
Benjamin Mason-Gallagher <BenMasonG@protonmail.com>
2022-07-23 23:16:27 +0100

    Amendments made to collatx conjecture code.

--- Numbers/collatz_conjecture.py
+++ Numbers/collatz_conjecture.py
@@ -16,15 +16,14 @@ def steps(number):
     Returns:
         int: the number of steps it takes for the number to be reduced to 1.
     """
-    
+
     if number < 1:
         raise ValueError("Only positive integers are allowed")
-    
+
     number_of_steps = 0
-    
+
     while number != 1:
         number = 3 * number + 1 if number % 2 else number / 2
         number_of_steps += 1
-        
+
     return number_of_steps
-    
